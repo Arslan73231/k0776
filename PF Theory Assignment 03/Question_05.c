69b6a9d072bc4604f1e2d31016ab854e8170c1d7
Arslan Rashid <arslanrashid066@gmail.com>
2024-12-08 14:38:33 +0500

    Update Question_05.c

--- PF Theory Assignment 03/Question_05.c
+++ PF Theory Assignment 03/Question_05.c
@@ -2,77 +2,85 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_SPECIES 10
-#define MAX_SUPPLIES 20
-char*** speciesSupplies;
-int speciesCount = 0;
-void initializeInventory(int count) {
-    speciesCount = count;
-    speciesSupplies = malloc(speciesCount * sizeof(char**));
-    for (int i = 0; i < speciesCount; i++) {
-        speciesSupplies[i] = malloc(MAX_SUPPLIES * sizeof(char*));
-        for (int j = 0; j < MAX_SUPPLIES; j++) {
-            speciesSupplies[i][j] = NULL;
+#define MAX_ITEMS 10
+#define MAX_STORAGE 20
+char*** categoryItems;
+int categoryCount = 0;
+
+void initializeStorage(int count) {
+    categoryCount = count;
+    categoryItems = malloc(categoryCount * sizeof(char**));
+    for (int i = 0; i < categoryCount; i++) {
+        categoryItems[i] = malloc(MAX_STORAGE * sizeof(char*));
+        for (int j = 0; j < MAX_STORAGE; j++) {
+            categoryItems[i][j] = NULL;
         }
     }
 }
-void addSupplies(int speciesIndex, char* supply) {
-    for (int j = 0; j < MAX_SUPPLIES; j++) {
-        if (speciesSupplies[speciesIndex][j] == NULL) {
-            speciesSupplies[speciesIndex][j] = malloc((strlen(supply) + 1) * sizeof(char));
-            strcpy(speciesSupplies[speciesIndex][j], supply);
+
+void addItem(int categoryIndex, char* item) {
+    for (int j = 0; j < MAX_STORAGE; j++) {
+        if (categoryItems[categoryIndex][j] == NULL) {
+            categoryItems[categoryIndex][j] = malloc((strlen(item) + 1) * sizeof(char));
+            strcpy(categoryItems[categoryIndex][j], item);
             break;
         }
     }
 }
-void updateSupplies(int speciesIndex, int supplyIndex, char* newSupply) {
-    if (speciesSupplies[speciesIndex][supplyIndex] != NULL) {
-        free(speciesSupplies[speciesIndex][supplyIndex]);
-        speciesSupplies[speciesIndex][supplyIndex] = malloc((strlen(newSupply) + 1) * sizeof(char));
-        strcpy(speciesSupplies[speciesIndex][supplyIndex], newSupply);
+
+void modifyItem(int categoryIndex, int itemIndex, char* updatedItem) {
+    if (categoryItems[categoryIndex][itemIndex] != NULL) {
+        free(categoryItems[categoryIndex][itemIndex]);
+        categoryItems[categoryIndex][itemIndex] = malloc((strlen(updatedItem) + 1) * sizeof(char));
+        strcpy(categoryItems[categoryIndex][itemIndex], updatedItem);
     }
-}
-void removeSpecies(int speciesIndex) {
-    for (int j = 0; j < MAX_SUPPLIES; j++) {
-        if (speciesSupplies[speciesIndex][j] != NULL) {
-            free(speciesSupplies[speciesIndex][j]);
-        }
-    }
-    free(speciesSupplies[speciesIndex]);
-    for (int i = speciesIndex; i < speciesCount - 1; i++) {
-        speciesSupplies[i] = speciesSupplies[i + 1];
-    }
-    speciesCount--;
-}
-void displayInventory() {
-    for (int i = 0; i < speciesCount; i++) {
-        printf("Species %d Supplies:\n", i + 1);
-        for (int j = 0; j < MAX_SUPPLIES; j++) {
-            if (speciesSupplies[i][j] != NULL) {
-                printf(" - %s\n", speciesSupplies[i][j]);
-            }
-        }
-    }
-}
-int main() {
-    initializeInventory(3);
-    addSupplies(0, "Food");
-    addSupplies(0, "Leash");
-    addSupplies(1, "Toys");
-    displayInventory();
-    updateSupplies(0, 0, "Dog Food");
-    displayInventory();
-    removeSpecies(1);
-    displayInventory();
-    for (int i = 0; i < speciesCount; i++) {
-        for (int j = 0; j < MAX_SUPPLIES; j++) {
-            if (speciesSupplies[i][j] != NULL) {
-                free(speciesSupplies[i][j]);
-            }
-        }
-        free(speciesSupplies[i]);
-    }
-    free(speciesSupplies);
-    return 0;
 }
 
+void deleteCategory(int categoryIndex) {
+    for (int j = 0; j < MAX_STORAGE; j++) {
+        if (categoryItems[categoryIndex][j] != NULL) {
+            free(categoryItems[categoryIndex][j]);
+        }
+    }
+    free(categoryItems[categoryIndex]);
+    for (int i = categoryIndex; i < categoryCount - 1; i++) {
+        categoryItems[i] = categoryItems[i + 1];
+    }
+    categoryCount--;
+}
+
+void displayStorage() {
+    for (int i = 0; i < categoryCount; i++) {
+        printf("Category %d Contents:\n", i + 1);
+        for (int j = 0; j < MAX_STORAGE; j++) {
+            if (categoryItems[i][j] != NULL) {
+                printf(" --> %s\n", categoryItems[i][j]);
+            }
+        }
+    }
+}
+
+int main() {
+    initializeStorage(3);
+    addItem(0, "Dry Food");
+    addItem(0, "Collar");
+    addItem(1, "Play Items");
+    displayStorage();
+
+    modifyItem(0, 0, "Special Dog Food");
+    displayStorage();
+
+    deleteCategory(1);
+    displayStorage();
+
+    for (int i = 0; i < categoryCount; i++) {
+        for (int j = 0; j < MAX_STORAGE; j++) {
+            if (categoryItems[i][j] != NULL) {
+                free(categoryItems[i][j]);
+            }
+        }
+        free(categoryItems[i]);
+    }
+    free(categoryItems);
+    return 0;
+}
